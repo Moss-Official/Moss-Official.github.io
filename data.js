d8867ae5cba790fc6629a220d67d67d6c0b9d735
Moss-Official <moss.companion@proton.me>
2026-06-28 11:18:20 +0300

    Update data.js

--- data.js
+++ data.js
@@ -47,58 +47,46 @@ const data = `
       {
         "clip_name": "Keep Up",
         "author": "Odetari",
-        "clip_dir": "Media/Clips/Keep_Up.mp4"
+        "clip_dir": "Media/Clips/Keep_Up.mp4",
+        "preview_dir": "Media/Previews/Keep_Up.png"
       }
     ]
   },
   "2025" : {
     "tracks": [
-    {
-      "track_name": "Galvanize",
-      "author": "The Chemical Brothers",
-      "track_cover_dir": "Media/Covers/Galvanize.jpg",
-      "track_dir": "Media/Tracks/Galvanize.mp3",
-      "person" : "Лёха",
-      "country" : "Великобритания",
-      "flag_dir" : "Media/Flags/UK.png",
-      "points" : 8
-    },
-    {
-      "track_name": "Keep Up",
-      "author": "Odetari",
-      "track_cover_dir": "Media/Covers/Keep_Up.jpg",
-      "track_dir": "Media/Tracks/Keep_Up.mp3",
-      "person" : "Макс",
-      "country" : "США",
-      "flag_dir" : "Media/Flags/USA.png",
-      "points" : 3
-    },
-    {
-      "track_name": "Dracula",
-      "author": "Tame Impala",
-      "track_cover_dir": "Media/Covers/Dracula.jpg",
-      "track_dir": "Media/Tracks/Dracula.mp3",
-      "person" : "Коля",
-      "country" : "Австралия",
-      "flag_dir" : "Media/Flags/Australia.png",
-      "points" : 9
-    },
-    {
-      "track_name": "Smack That",
-      "author": "Eminem",
-      "track_cover_dir": "Media/Covers/Smack_that.jpg",
-      "track_dir": "Media/Tracks/Smack_that.mp3",
-      "person" : "Витя",
-      "country" : "США",
-      "flag_dir" : "Media/Flags/USA.png",
-      "points" : 6
-    }
+      {
+        "track_name": "Gossip",
+        "author": "Måneskin",
+        "track_cover_dir": "Media/Covers/2025/Gossip.jpg",
+        "track_dir": "Media/Tracks/2025/Maneskin-Gossip.mp3",
+        "person" : "Schrödinger's Rodion",
+        "country" : "Италия",
+        "flag_dir" : "Media/Flags/Italy.png",
+        "points" : 63
+      },
+      {
+        "track_name": "Under Your Spell",
+        "author": "Snow Strippers",
+        "track_cover_dir": "Media/Covers/2025/Under_Your_Spell.jpg",
+        "track_dir": "Media/Tracks/2025/Snow_Strippers-Under_Your_Spell.mp3",
+        "person" : "Takos",
+        "country" : "США",
+        "flag_dir" : "Media/Flags/USA.png",
+        "points" : 61
+      }
     ],
     "clips": [
       {
-        "clip_name": "Keep Up",
-        "author": "Odetari",
-        "clip_dir": "Media/Clips/Keep_Up.mp4"
+        "clip_name": "Gossip",
+        "author": "Måneskin",
+        "clip_dir": "Media/Clips/2025/Gossip.mp4",
+        "preview_dir": "Media/Previews/2025/Gossip.png"
+      },
+      {
+        "clip_name": "Under Your Spell",
+        "author": "Snow Strippers",
+        "clip_dir": "Media/Clips/2025/Under_Your_Spell.mp4",
+        "preview_dir": "Media/Previews/2025/Under_Your_Spell.png"
       }
     ]
   },
